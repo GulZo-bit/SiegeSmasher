d4bb92230c63ed20879fdd31a0b355fdf843294d
JRRoddy <jamesrichardroddy@hotmail.co.uk>
2025-10-05 22:33:29 +0100

    Reapply "Tower placement system"
    This reverts commit 283c3b05d09458e1b14cf4c277bae80f8100732b.

---
+++ SiegeSmasher/Intermediate/Build/Win64/UnrealEditor/Inc/SiegeSmasher/UHT/TowerTest.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Towers/TowerTest.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SIEGESMASHER_TowerTest_generated_h
+#error "TowerTest.generated.h already included, missing '#pragma once' in TowerTest.h"
+#endif
+#define SIEGESMASHER_TowerTest_generated_h
+
+#define FID_Users_richa_OneDrive_Desktop_SiegeSmasher_SiegeSmasher_SiegeSmasher_Source_SiegeSmasher_Towers_TowerTest_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATowerTest(); \
+	friend struct Z_Construct_UClass_ATowerTest_Statics; \
+public: \
+	DECLARE_CLASS(ATowerTest, ATowerBase, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SiegeSmasher"), NO_API) \
+	DECLARE_SERIALIZER(ATowerTest)
+
+
+#define FID_Users_richa_OneDrive_Desktop_SiegeSmasher_SiegeSmasher_SiegeSmasher_Source_SiegeSmasher_Towers_TowerTest_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ATowerTest(ATowerTest&&); \
+	ATowerTest(const ATowerTest&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATowerTest); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATowerTest); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATowerTest) \
+	NO_API virtual ~ATowerTest();
+
+
+#define FID_Users_richa_OneDrive_Desktop_SiegeSmasher_SiegeSmasher_SiegeSmasher_Source_SiegeSmasher_Towers_TowerTest_h_10_PROLOG
+#define FID_Users_richa_OneDrive_Desktop_SiegeSmasher_SiegeSmasher_SiegeSmasher_Source_SiegeSmasher_Towers_TowerTest_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_richa_OneDrive_Desktop_SiegeSmasher_SiegeSmasher_SiegeSmasher_Source_SiegeSmasher_Towers_TowerTest_h_13_INCLASS_NO_PURE_DECLS \
+	FID_Users_richa_OneDrive_Desktop_SiegeSmasher_SiegeSmasher_SiegeSmasher_Source_SiegeSmasher_Towers_TowerTest_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SIEGESMASHER_API UClass* StaticClass<class ATowerTest>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_richa_OneDrive_Desktop_SiegeSmasher_SiegeSmasher_SiegeSmasher_Source_SiegeSmasher_Towers_TowerTest_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
