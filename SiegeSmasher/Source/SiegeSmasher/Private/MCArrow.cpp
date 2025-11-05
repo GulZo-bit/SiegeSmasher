4cd7962ac98c4f2891253d68535f79c5d7b108db
Ktos04 <kewinbinias04@gmail.com>
2025-11-05 10:53:57 +0000

    Added sound to the arrow (needs testing on multiple devices)

--- SiegeSmasher/Source/SiegeSmasher/Private/MCArrow.cpp
+++ SiegeSmasher/Source/SiegeSmasher/Private/MCArrow.cpp
@@ -31,6 +31,12 @@ AMCArrow::AMCArrow()
 		RootComponent = ArrowCollision;
 	}
 
+	if (!SoundTrigger) 
+	{
+		SoundTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("SoundTrigger"));
+		SoundTrigger->SetupAttachment(RootComponent);
+	}
+
 	if (!ArrowMovement)
 	{
 		// Movement component controls the Arrows movement properties like speed, gravity etc
@@ -74,6 +80,8 @@ void AMCArrow::BeginPlay()
 {
 	Super::BeginPlay();
 	ArrowMesh->SetMaterial(0, ArrowMaterialInstance);
+
+	SoundTrigger->OnComponentBeginOverlap.AddDynamic(this, &AMCArrow::ArrowOverlap);
 }
 
 // Called every frame
@@ -87,6 +95,15 @@ void AMCArrow::Tick(float DeltaTime)
 void AMCArrow::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
 {
 	Destroy();
+}
+
+void AMCArrow::ArrowOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (WhooshingSound != nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Yellow, FString::Printf(TEXT("Arrow Overlapped Player")));
+		UGameplayStatics::PlaySoundAtLocation(this, WhooshingSound, GetActorLocation());
+	}
 }
 
 void AMCArrow::FireInDirection(const FVector& ShootDirection, float Charge)
--- SiegeSmasher/Source/SiegeSmasher/Public/MCArrow.h
+++ SiegeSmasher/Source/SiegeSmasher/Public/MCArrow.h
@@ -8,6 +8,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Net/UnrealNetwork.h"
+#include "Kismet/GameplayStatics.h"
 #include "MCArrow.generated.h"
 
 UCLASS()
@@ -29,7 +30,8 @@ public:
 
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
-
+	UFUNCTION()
+	void ArrowOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	//Sphere collision component, will be used to detect when the projectile hits something
 	UPROPERTY(VisibleDefaultsOnly, Category = "Projectile")
 	USphereComponent* ArrowCollision;
@@ -40,6 +42,11 @@ public:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Movement")
 	UProjectileMovementComponent* ArrowMovement;
 
+	UPROPERTY(VisibleDefaultsOnly, Category = "Projectile")
+	USphereComponent* SoundTrigger;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	USoundBase* WhooshingSound;
 
 
 	// Projectile material
