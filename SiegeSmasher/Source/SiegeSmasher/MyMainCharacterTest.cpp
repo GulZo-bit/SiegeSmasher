842b09341703160d8eceffc39e242b824d9be856
Ktos04 <kewinbinias04@gmail.com>
2025-11-25 16:54:25 +0000

    Added a commit at the top of character cpp

--- SiegeSmasher/Source/SiegeSmasher/MyMainCharacterTest.cpp
+++ SiegeSmasher/Source/SiegeSmasher/MyMainCharacterTest.cpp
@@ -12,7 +12,7 @@ AMainCharacterTest::AMainCharacterTest()
 {
 	TowerSpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
-
+	//Test comment for commit
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
